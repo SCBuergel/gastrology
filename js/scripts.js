4fb54acc5b3ada80f28adb034f215a7bfe1f1826
Sebastian Bürgel <s.buergel@gmail.com>
2020-11-23 23:14:52 +0100

    removed web3, wrapped web3

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,21 @@
+// based on https://ethereum.stackexchange.com/a/24238
+
+const proxiedWeb3Handler = {
+  get: (target, name) => {              
+    const inner = target[name];                            
+    if (inner instanceof Function) {                       
+      return (...args) => promisify(cb => inner(...args, cb));                                                         
+    } else if (typeof inner === 'object') {                
+      return new Proxy(inner, proxiedWeb3Handler);
+    } else {
+      return inner;
+    }
+  },
+};
+
 window.onload = function() {
-	var myDiv = document.createElement("div");
-  myDiv.innerHTML = "JS up and running";
+	let blockNumber = await proxiedWeb3.eth.getBlockNumber();
+  var myDiv = document.createElement("div");
+  myDiv.innerText = "Latest block: " + blockNumber;
   document.body.appendChild(myDiv);
 }
