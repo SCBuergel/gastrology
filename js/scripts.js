5c571ac3ae8cad8d4073e8578fce3fbd65f3ce84
Sebastian Bürgel <s.buergel@gmail.com>
2020-11-24 22:41:33 +0100

    fixed highest / lowest

--- js/scripts.js
+++ js/scripts.js
@@ -91,12 +91,12 @@ async function loadBlocks() {
 		}));
 		
 		blockTxs.sort((a,b)=>a-b);
-		let tenthHighestGas = blockTxs.length > 20 ? blockTxs[9] : "-";
+		let tenthLowestGas = blockTxs.length > 20 ? blockTxs[9] : "-";
 		let minGas = blockTxs.length > 0 ? Math.min(...blockTxs) : "-";
 		let medianGas = blockTxs.length > 0 ? median(blockTxs) : "-";
 		let averageGas = blockTxs.length > 0 ? average(blockTxs) : "-";
 		blockTxs.sort((a,b)=>b-a);
-		let tenthLowestGas = blockTxs.length > 20 ? blockTxs[9] : "-";
+		let tenthHighestGas = blockTxs.length > 20 ? blockTxs[9] : "-";
 		let maxGas = blockTxs.length > 0 ? Math.max(...blockTxs) : "-";
 
 		var row = table.insertRow();
