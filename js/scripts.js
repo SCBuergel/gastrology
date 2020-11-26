e9791a25d637f16bda0831ac7d1a7ada773aad1e
Sebastian Bürgel <s.buergel@gmail.com>
2020-11-26 15:42:08 +0100

    debug

--- js/scripts.js
+++ js/scripts.js
@@ -59,6 +59,7 @@ async function toggle() {
 
 function findSmallestNonZero(data) {
 	let smallest = Number.MAX_SAFE_INTEGER;
+	console.log("processing smallest, length = " + data.length);
 	for (let c = 0; c < data.length; c++) {
 		if (data[c] < smallest) {
 		  console.log("new smallest: " + data[c]);
